d8b17a9ce36b32f99bdeee3664320ffeb6cf2d3c
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-06 15:36:28 +0300

    Implemented the palindrome api

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+     public function palindrome(){
+        $count=0;
+        $palindromes = array("level", "radar", "sweet","lolol");
+                $N=count($palindromes);
+
+        for ($x = 0; $x <$N; $x++) {
+             $l = 0;
+             $r = strlen($palindromes[$x]) - 1;
+             $flag = 0;
+
+            while($r > $l){
+                if ($palindromes[$x][$l] != $palindromes[$x][$r]){
+                    $flag = 1;
+                     break;
+                 }
+             $l++;
+             $r--;
+            }
+
+            if ($flag == 0){
+                $count+=1;
+            }
+
+    }
+        echo($count);
+  }
+
+}
